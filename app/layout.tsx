7bfd0e4a031d73a9dbc999c02db06216ea4065ef
it5_Web <6821901024@cdti.ac.th>
2026-06-15 10:32:06 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+
+import Footer from "./component/Footer ";
+import Navbar from "./component/Navbar";
+import "./globals.css";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body >
+        <Navbar/>
+       
+        {children}
+        <Footer/>
+        </body>
+    </html>
+  );
+}
