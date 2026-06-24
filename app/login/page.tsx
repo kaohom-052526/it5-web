22664fd24d75575615255f9bf7370d3212374650
it5_Web <6821901024@cdti.ac.th>
2026-06-24 10:17:23 +0700

    upudate

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,15 @@
-eexport default function Login() {
+export default function Login() {
   return (
     <div className="auth-page">
-      <form className="auth-card"
-       <h1> เขาสู่ระบบ </h1>
 
+     <form className="auth-card">
+       <h1> เข้าสู่ระบบ </h1>
+       <input placeholder="Email"/>
+       <input placeholder="Password" type="password"/>
+       <button> Login </button>
+
+      </form>
     </div>
+
   );
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,16 +1,52 @@
-export default function Login() {
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function RegisterPage() {
+  const router = useRouter();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify(form),
+    });
+
+    if (res.ok) {
+      router.push("/login");
+    } else {
+      alert("Register failed");
+    }
+  }
+
   return (
     <div className="auth-page">
-      <form className="auth-card"
-       <h1> สมัครสมาชิก </h1>
-        <input placeholder= "ชื่อ นามสกุล" />
-        <input placeholdre= "Email"  />
-        <input placeholdre= "Password"  />
+      <form className="auth-card" onSubmit={handleSubmit}>
+        <h1>Register</h1>
 
-        <input placeholdre= "Register"  />
+        <input
+          placeholder="Name"
+          onChange={(e) => setForm({ ...form, name: e.target.value })}/>
 
-     </form>
+        <input
+          placeholder="Email" type="email"
+          onChange={(e) => setForm({ ...form, email: e.target.value })}/>
+
+        <input
+          placeholder="Password" type="password"
+          onChange={(e) => setForm({ ...form, password: e.target.value })}/>
+
+        <button>Register</button>
+        
+      </form>
     </div>
-
   );
 }
